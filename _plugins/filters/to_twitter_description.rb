0630881e7908a12cc60eb9e6f2b004a60b569464
Andrew Dunkman <adunkman@getharvest.com>
2013-12-01 21:46:00 -0500

    Strip liquid tags from twitter descriptions.

--- _plugins/filters/to_twitter_description.rb
+++ _plugins/filters/to_twitter_description.rb
@@ -1,7 +1,11 @@
 module Jekyll
   module TwitterCard
     def to_twitter_description(markdown)
-      strip_html(markdownify(markdown))
+      strip_liquid(strip_html(markdownify(markdown)))
+    end
+
+    def strip_liquid(input)
+      input.gsub(/\{%[^%]*%\}/, "")
     end
   end
 end
