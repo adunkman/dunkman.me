da39aa2b0041bad7427d1a009ba70d0cd6a2099d
Andrew Dunkman <adunkman@gmail.com>
2020-08-07 19:49:34 -0400

    Use S3 + DynamoDB backend for terraform.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "dunkman.me-terraform"
+    key = "state"
+    region = "us-east-1"
+    dynamodb_table = "terraform"
+  }
+}
+
 provider "aws" {
   version = "~> 3.0"
   region = "us-east-1"
