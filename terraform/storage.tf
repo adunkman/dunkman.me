1643a0f2fbca626fed1d21563cb09833ab9cb5ec
Andrew Dunkman <adunkman@gmail.com>
2020-08-14 00:32:21 -0400

    Handle root directory requests.
    Previously results in Access Denied error.

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,6 +1,11 @@
 resource "aws_s3_bucket" "dunkman_me" {
   bucket = "dunkman.me"
   acl = "private"
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "dunkman_me" {
