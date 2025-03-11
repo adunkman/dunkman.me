e42e052d2544a572aedbf33bb3d377674d8b8f60
Andrew Dunkman <adunkman@gmail.com>
2025-03-11 11:04:05 -0400

    Switch user agents
    It appears this actually increased my failure rate

--- metascraper/requestOptions.js
+++ metascraper/requestOptions.js
@@ -1,6 +1,3 @@
 export default {
-  headers: {
-    "User-Agent": "Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Chrome/41.0.2272.96 Safari/537.36"
-  },
   timeout: 10000
 };
